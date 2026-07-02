cb027a83e139d25083138340312c2c130fc7e990
nasnasrich <nasnasrich@gmail.com>
2026-07-02 12:12:28 -0700

    commit

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
-//import cors from "cors"
+import cors from "cors";
 import { configDotenv } from "dotenv";
 import userRoute from "./routes/userRoute.js";
 
-configDotenv()
+configDotenv();
 const app = express();
 app.use(express.json());
-//app.use(cors());
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
 // Routes
-app.use('/api/auth', userRoute);
-
-
+app.use("/api/auth", userRoute);
 
 app.get("/", (req, res) => {
   res.send("API IS WORKING FINE!");
@@ -23,15 +28,12 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-
 const connectDB = async () => {
-    try {
-       await mongoose.connect(process.env.MONGO_URI, {
-       
-      });
-      console.log("MongoDB connected successfully"); 
-    } catch (error) {
-      console.error("Error connecting to MongoDB:", error);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {});
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
 connectDB();
