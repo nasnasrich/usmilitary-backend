285538048bbb10c6e0f7455fa585264181a7ac81
nasnasrich <nasnasrich@gmail.com>
2026-07-10 23:36:11 -0700

    local

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin:["http://localhost:5174/auth",
+    origin:["http://localhost:5173",
       "https://usmilitary-frontend-2.vercel.app"],
      methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
