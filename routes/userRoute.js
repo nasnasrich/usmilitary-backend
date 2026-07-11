0e44aa5885c7c54031db61378a2d010f0ed5677d
nasnasrich <nasnasrich@gmail.com>
2026-07-10 23:21:17 -0700

    Notice there is no / at the end

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createUser, loginUser, getAllUsers, getUserById, updateUser, deleteUser } from "../controllers/userController.js";
 
 const router = express.Router();
+
 router.post('/register', createUser)
 router.post('/login', loginUser)
 router.put('/update/:id', updateUser)
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(
   cors({
     origin:["http://localhost:5173",
-      "https://usmilitary-frontend-2.vercel.app/"],
+      "https://usmilitary-frontend-2.vercel.app"],
      methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
