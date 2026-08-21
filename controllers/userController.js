62be90ff116e79e0036fac0a5fa046679ea4df41
nasnasrich <nasnasrich@gmail.com>
2026-08-21 16:22:53 -0700

    SMTP server transporter

--- controllers/userController.js
+++ controllers/userController.js
@@ -174,12 +174,14 @@ export const forgotPassword = async (req, res) => {
 
     // Create email transporter
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+        });
 
     // Email
     const mailOptions = {
